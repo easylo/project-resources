32ed59f431813ba10ea0c0982c896f2f3e484938
Laurent RICHARD <lrichard@lafourchette.com>
2020-05-12 12:02:12 +0200

    add logos

---
+++ public/logos/resize.sh
@@ -0,0 +1,40 @@
+
+
+#ls -l | grep ^- | awk -v x=9 '{convert $x -resize 200x200 $x }'
+#convert GitLab_logo.png -resize 200x200 GitLab_logo.png
+
+convert GitLab_logo.png -resize 200x200 GitLab_logo.png
+convert NGINX.png -resize 200x200 NGINX.png
+convert anemometer.png -resize 200x200 anemometer.png
+convert aws.png -resize 200x200 aws.png
+convert backoffice.png -resize 200x200 backoffice.png
+convert bookmark.png -resize 200x200 bookmark.png
+convert box_shadow.png -resize 200x200 box_shadow.png
+convert catchpoint.png -resize 200x200 catchpoint.png
+convert commingsoon.jpg -resize 200x200 commingsoon.jpg
+convert confluence.png -resize 200x200 confluence.png
+convert dashing.png -resize 200x200 dashing.png
+convert datawarehouse.png -resize 200x200 datawarehouse.png
+convert elasticsearch.png -resize 200x200 elasticsearch.png
+convert footer_bots.png -resize 200x200 footer_bots.png
+convert fork.png -resize 200x200 fork.png
+convert github.png -resize 200x200 github.png
+convert gitlab.png -resize 200x200 gitlab.png
+convert grafana.png -resize 200x200 grafana.png
+convert hulkfork.png -resize 200x200 hulkfork.png
+convert jenkins.png -resize 200x200 jenkins.png
+convert jira.png -resize 200x200 jira.png
+convert kibana.png -resize 200x200 kibana.png
+convert koala.png -resize 200x200 koala.png
+convert logo.png -resize 200x200 logo.png
+convert phpldapadmin.png -resize 200x200 phpldapadmin.png
+convert prometheus.png -resize 200x200 prometheus.png
+convert rabbitmq.png -resize 200x200 rabbitmq.png
+convert rancher.png -resize 200x200 rancher.png
+convert rundeck.png -resize 200x200 rundeck.png
+convert selenium.png -resize 200x200 selenium.png
+convert statuscake.png -resize 200x200 statuscake.png
+convert superset.png -resize 200x200 superset.png
+convert translate.png -resize 200x200 translate.png
+convert vault.png -resize 200x200 vault.png
+convert webistrano.png -resize 200x200 webistrano.png
